bead0054362f28b4dc06d4780a331160194a9360
unknown <deathrow1@.influx.net>
2009-06-16 01:54:04 +0200

    Removed some stuff, but didn't test to see if it works
    Should remove threshold.c too, I don't think it's referenced

--- definitions.h
+++ definitions.h
@@ -9,25 +9,15 @@
 
 #define PI 3.141592653
 
-// Defined externally
+// Defined externally at compilation
 //#define MAX_NO_PARTICLES  15000  /* Maximum number of particles/processor */
-//#define INIT_NO_PARTICLES 500    /* Initial number of particles/processor */
+//#define INIT_NO_PARTICLES 10000    /* Initial number of particles/processor */
 
 #define MAX_INITIAL_VELOCITY 50
 
 #define BOX_HORIZ_SIZE 10000.0
 #define BOX_VERT_SIZE 10000.0
 #define WALL_LENGTH (2.0*BOX_HORIZ_SIZE+2.0*BOX_VERT_SIZE)
-
-#define PARTICLE_BUFFER_SIZE MAX_NO_PARTICLES/5
-#define COMM_BUFFER_SIZE  5*PARTICLE_BUFFER_SIZE
-
-struct particle {
-	pcord_t  pcord;
-	int ptype;        /* Used to simulate mixing of gases */
-};
-
-typedef struct particle particle_t;
 
 #define TIME_STEPS 10
 #define TIME_STEP 1
